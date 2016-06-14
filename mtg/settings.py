161283d467c98f74834a7efc47e58f20915d63b4
Jeff Richards <jeff.richards@earthlink.net>
2016-06-14 19:13:35 -0400

    Heroku static try 2

--- mtg/settings.py
+++ mtg/settings.py
@@ -115,7 +115,7 @@ USE_X_FORWARDED_HOST = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 # Additional locations of static files
 STATICFILES_DIRS = (

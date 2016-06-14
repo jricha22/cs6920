fbef94458b83fee46afa638b7caabca6858ffff2
Cory Alford <calford4@my.westga.edu>
2016-06-13 18:42:23 -0700

    Calls to the website's api to get json data to populate the table. Cleaned up CSS for  nav bar.

--- front_end/app/app.js
+++ front_end/app/app.js
@@ -5,7 +5,9 @@ angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
-  'myApp.version'
+  'myApp.version',
+  'myApp.services',
+  'ngResource'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
--- front_end/app/view1/view1.js
+++ front_end/app/view1/view1.js
@@ -1,14 +1,33 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+angular.module('myApp.view1', ['ngRoute', 'ngResource'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
     templateUrl: 'static/view1/view1.html',
-    controller: 'View1Ctrl'
+    controller: 'CardCtrl1'
   });
 }])
 
 .controller('View1Ctrl', [function() {
 
+}])
+
+.controller('CardCtrl1', ['$scope', 'MTGCards', function($scope, MTGCards) {
+  // Instantiate an object to store scope data in
+  $scope.data = {};
+
+  MTGCards.get(function(response) {
+    // Assign the response INSIDE the callback
+    $scope.data.cards = response.results;
+    console.log(response)
+  });
 }]);
+
+
+// angular-resource for getting JSON data from the MTG API
+angular.module('myApp.services', ['ngResource'])
+  .factory('MTGCards', function($resource){
+      //change resource url in production
+    return $resource('/api/collect/card/', {})
+});

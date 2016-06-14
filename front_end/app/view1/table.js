37868d0a8147cf55168678c6fda84ac4dc4bef42
Cory Alford <calford4@my.westga.edu>
2016-06-13 23:19:30 -0700

    Updated navbar using bootstrap. Cleaned up script tags to reference external urls.

---
+++ front_end/app/view1/table.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Cory on 6/13/2016.
+ */
+$(document).ready(function() {
+    // Setup - add a text input to each footer cell
+    $('#card_browse tfoot th').each( function () {
+        var title = $(this).text();
+        $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
+    } );
+
+    // DataTable
+    var table = $('#card_browse').DataTable();
+
+    // Apply the search
+    table.columns().every( function () {
+        var that = this;
+
+        $( 'input', this.footer() ).on( 'keyup change', function () {
+            if ( that.search() !== this.value ) {
+                that.search( this.value ).draw();
+            }
+        } );
+    } );
+} );

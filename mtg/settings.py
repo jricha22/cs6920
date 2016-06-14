e76c95ef66bb6254de68cf61edd9bba8ac2c7536
Jeff Richards <jeff.richards@earthlink.net>
2016-06-14 19:11:11 -0400

    Static files settings for Heroku.  Note: every experiment with Heroku requires a commit, thus a flood of micro commits is required while getting Heroku going

--- mtg/settings.py
+++ mtg/settings.py
@@ -115,6 +115,7 @@ USE_X_FORWARDED_HOST = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 # Additional locations of static files
 STATICFILES_DIRS = (
@@ -123,6 +124,10 @@ STATICFILES_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
 )
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 API_MODULES = {
     'collect',

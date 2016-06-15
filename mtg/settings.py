f3a78e6fa1973a1f1fddebb1bdfa9ac947d09156
Jeff Richards <jeff.richards@earthlink.net>
2016-06-14 22:46:00 -0400

    Merge branch 'master' of https://github.com/jricha22/cs6920
    # Conflicts:
    #	front_end/app/index.html

--- mtg/settings.py
+++ mtg/settings.py
@@ -97,9 +97,10 @@ DATABASES = {
     }
 }
 
-# Update database configuration with $DATABASE_URL.
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+if os.environ.get('HEROKU'):
+    # Update database configuration with $DATABASE_URL.
+    db_from_env = dj_database_url.config(conn_max_age=500)
+    DATABASES['default'].update(db_from_env)
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')

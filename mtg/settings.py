3988f80171bdc1c0f289044307860cc19799fd5d
Jeff Richards <jeff.richards@earthlink.net>
2016-06-14 21:53:23 -0400

    Heroku flailing more with whitenoise...

--- mtg/settings.py
+++ mtg/settings.py
@@ -115,6 +115,7 @@ USE_X_FORWARDED_HOST = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 # Additional locations of static files
 STATICFILES_DIRS = (

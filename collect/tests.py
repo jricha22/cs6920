ffaccd2a474ff12189f034710b3c1ed8100dd709
Jeff Richards <jeff.richards@earthlink.net>
2016-06-16 22:08:20 -0400

    Services unit tests for core and collect.  Run they with python manage.py test.

--- collect/tests.py
+++ collect/tests.py
@@ -1,3 +1,64 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase, APIClient
+from collect.models import *
 
-# Create your tests here.
+
+class CardTest(APITestCase):
+    def setUp(self):
+        self.superuser = User.objects.create_superuser('jdoe', 'o@o.com', 'pass1234')
+        self.client = APIClient()
+        self.client.login(username='jdoe', password='pass1234')
+        self.card = Card.objects.get(id=10)
+        self.card_ten = Card.objects.all()[10]
+
+    def test_card_list(self):
+        response = self.client.get(reverse('card-list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(5, len(response.data['results']))
+
+    def test_card_detail(self):
+        response = self.client.get(reverse('card-detail', args=[self.card.id]))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(self.card.name, response.data['name'])
+
+    def test_card_pagination_length(self):
+        response = self.client.get(reverse('card-list'), {'limit': 10})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(10, len(response.data['results']))
+
+    def test_card_pagination_offset(self):
+        response = self.client.get(reverse('card-list'), {'offset': 10})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(self.card_ten.name, response.data['results'][0]['name'])
+
+
+class ManaCostTest(APITestCase):
+    def setUp(self):
+        self.superuser = User.objects.create_superuser('jdoe', 'o@o.com', 'pass1234')
+        self.client = APIClient()
+        self.client.login(username='jdoe', password='pass1234')
+        self.cost = ManaCost.objects.get(id=10)
+        self.cost_ten = ManaCost.objects.all()[10]
+
+    def test_cost_list(self):
+        response = self.client.get(reverse('manacost-list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(5, len(response.data['results']))
+
+    def test_cost_detail(self):
+        response = self.client.get(reverse('manacost-detail', args=[self.cost.id]))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(self.cost.color, response.data['color'])
+        self.assertEqual(self.cost.count, response.data['count'])
+
+    def test_cost_pagination_length(self):
+        response = self.client.get(reverse('manacost-list'), {'limit': 10})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(10, len(response.data['results']))
+
+    def test_cost_pagination_offset(self):
+        response = self.client.get(reverse('manacost-list'), {'offset': 10})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(self.cost_ten.color, response.data['results'][0]['color'])
--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,54 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase, APIClient
 
-# Create your tests here.
+
+class UserTest(APITestCase):
+    def setUp(self):
+        self.superuser = User.objects.create_superuser('jdoe', 'o@o.com', 'pass1234')
+        self.client = APIClient()
+        self.client.login(username='jdoe', password='pass1234')
+        self.user = User.objects.create(username="tsmith")
+
+    def test_user_list(self):
+        response = self.client.get(reverse('user-list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(2, len(response.data['results']))
+
+    def test_user_detail(self):
+        response = self.client.get(reverse('user-detail', args=[self.user.id]))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual('tsmith', response.data['username'])
+
+
+class ProfileTest(APITestCase):
+    def setUp(self):
+        self.superuser = User.objects.create_superuser('jdoe', 'o@o.com', 'pass1234')
+        self.client = APIClient()
+
+    def test_user_not_logged_in(self):
+        response = self.client.get(reverse('profile'))
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_user_logged_in(self):
+        self.client.login(username='jdoe', password='pass1234')
+        response = self.client.get(reverse('profile'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual('jdoe', response.data['username'])
+
+
+class LoginTest(APITestCase):
+    def setUp(self):
+        self.superuser = User.objects.create_superuser('jdoe', 'o@o.com', 'pass1234')
+        self.client = APIClient()
+
+    def test_credentials_good(self):
+        response = self.client.post(reverse('login'), {'username': 'jdoe', 'password': 'pass1234'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual('jdoe', response.data['username'])
+
+    def test_credentials_bad(self):
+        response = self.client.post(reverse('login'), {'username': 'nobody', 'password': 'idonthaveone'})
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+

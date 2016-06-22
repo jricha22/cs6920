cce367531f65b3f06311b803f0a158c74327251f
Cory Alford <calford4@my.westga.edu>
2016-06-21 21:04:18 -0700

    Removed search bar, removed all but one column sort. Added beginning of unit testing for view1

--- front_end/app/view1/view1_test.js
+++ front_end/app/view1/view1_test.js
@@ -2,15 +2,75 @@
 
 describe('myApp.view1 module', function() {
 
- beforeEach(module('myApp.view1'));
+  var $rootScope;
+  var $controller;
+  var scope, ctrl;
+  beforeEach(module('myApp.view1'));
+
+  beforeEach(inject(function($injector) {
+
+    $rootScope = $injector.get('$rootScope');
+    $controller = $injector.get('$controller');
+    scope = $rootScope.$new();
+    ctrl = $controller('PaginationCtrl', { $scope: scope});
+
+  }));
 
   describe('view1 controller', function(){
 
-    it('should ....', inject(function($controller) {
+    it('should have a defined controller', inject(function($controller) {
       //spec body
-      var view1Ctrl = $controller('View1Ctrl');
-      expect(view1Ctrl).toBeDefined();
+      var PaginationCtrl = $controller('PaginationCtrl');
+      expect(PaginationCtrl).toBeDefined();
     }));
 
+
   });
+
+  describe('color checkboxes', function(){
+
+      it('should change state', function() {
+          var value1 = element(by.binding('filterOptions.filterColor.Colorless'));
+          var value2 = element(by.binding('filterOptions.filterColor.Black'));
+          var value3 = element(by.binding('filterOptions.filterColor.Blue'));
+          var value4 = element(by.binding('filterOptions.filterColor.White'));
+          var value5 = element(by.binding('filterOptions.filterColor.Red'));
+          var value6 = element(by.binding('filterOptions.filterColor.Green'));
+    
+          expect(value1.getText()).toContain('false');
+          expect(value2.getText()).toContain('false');
+          expect(value3.getText()).toContain('false');
+          expect(value4.getText()).toContain('false');
+          expect(value5.getText()).toContain('false');
+          expect(value6.getText()).toContain('false');
+    
+          element(by.model('filterOptions.filterColor.Colorless')).click();
+          element(by.model('filterOptions.filterColor.Blue')).click();
+          element(by.model('filterOptions.filterColor.White')).click();
+          element(by.model('filterOptions.filterColor.Green')).click();
+    
+          expect(value1.getText()).toContain('true');
+          expect(value2.getText()).toContain('false');
+          expect(value3.getText()).toContain('true');
+          expect(value4.getText()).toContain('true');
+          expect(value5.getText()).toContain('false');
+          expect(value6.getText()).toContain('true');
+      });
+    });
+    
+    describe('mana limit number spinner', function(){
+
+        it('should have initial value', function() {
+            var value1 = element(by.binding('filterOptions.filterMana'));
+            expect(value1.getText()).toContain('15');
+        });
+
+        it('should change value', function() {
+            var value1 = element(by.binding('filterOptions.filterMana'));
+         //   element(by.model('filterOptions.filterMana')).setText('3');
+            expect(value1.getText()).toContain('3');
+        });
+
+    });
+    
 });

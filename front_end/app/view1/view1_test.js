a7341901564e3172e474dd6710354a845ad6d292
Jeff Richards <jeff.richards@earthlink.net>
2016-06-24 22:57:33 -0400

    Tests for manalimit and color combinations added

--- front_end/app/view1/view1_test.js
+++ front_end/app/view1/view1_test.js
@@ -2,13 +2,11 @@
 
 describe('myApp.view1 module', function() {
   beforeEach(module('myApp.view1'));
-  var pageCtrl, $scope;
+  var pageCtrl, $scope, httpBackend;
   describe('view1 controller', function(){
 
     it('should have a defined controller', function($controller) {
-        //var $scope = {}
-        //var $http = {}
-      var PaginationCtrl = $controller('PaginationCtrl');
+      pageCtrl = $controller('PaginationCtrl');
       expect(PaginationCtrl).toBeDefined();
     });
 
@@ -26,120 +24,89 @@ describe('myApp.view1 module', function() {
           expect($scope.totalServerItems).toEqual(3);
       });
   });
-/*
-  describe('color checkboxes', function(){
 
-      it('should change state', function() {
-          var value1 = element(by.binding('filterOptions.filterColor.Colorless'));
-          var value2 = element(by.binding('filterOptions.filterColor.Black'));
-          var value3 = element(by.binding('filterOptions.filterColor.Blue'));
-          var value4 = element(by.binding('filterOptions.filterColor.White'));
-          var value5 = element(by.binding('filterOptions.filterColor.Red'));
-          var value6 = element(by.binding('filterOptions.filterColor.Green'));
+  describe('Controller manalimit', function() {
+      beforeEach(inject(function ($controller, $rootScope, $httpBackend) {
+        $scope = $rootScope.$new();
+        httpBackend = $httpBackend;
+        httpBackend.when("GET", "/api/collect/card/?limit=10&offset=0&manalimit=15").respond({"results": [{}, {}, {}], "count": 1});
+        pageCtrl = $controller('PaginationCtrl', {$scope: $scope });
+        httpBackend.flush();
+      }));
 
-          expect(value1.getText()).toContain('false');
-          expect(value2.getText()).toContain('false');
-          expect(value3.getText()).toContain('false');
-          expect(value4.getText()).toContain('false');
-          expect(value5.getText()).toContain('false');
-          expect(value6.getText()).toContain('false');
-
-          element(by.model('filterOptions.filterColor.Colorless')).click();
-          element(by.model('filterOptions.filterColor.Blue')).click();
-          element(by.model('filterOptions.filterColor.White')).click();
-          element(by.model('filterOptions.filterColor.Green')).click();
-
-          expect(value1.getText()).toContain('true');
-          expect(value2.getText()).toContain('false');
-          expect(value3.getText()).toContain('true');
-          expect(value4.getText()).toContain('true');
-          expect(value5.getText()).toContain('false');
-          expect(value6.getText()).toContain('true');
+      it('should have 1 cards with manalimit 1', function () {
+          httpBackend.when("GET", "/api/collect/card/?limit=10&offset=0&manalimit=1").respond({"results": [{}], "count": 1});
+          $scope.filterOptions.filterMana = 1;
+          $scope.updateCards();
+          httpBackend.flush();
+          expect($scope.totalServerItems).toEqual(1);
       });
-    });
+  });
 
-    describe('mana limit number spinner', function(){
+  describe('Controller color filters', function() {
+      beforeEach(inject(function ($controller, $rootScope, $httpBackend) {
+        $scope = $rootScope.$new();
+        httpBackend = $httpBackend;
+        httpBackend.when("GET", "/api/collect/card/?limit=10&offset=0&manalimit=15").respond({"results": [{}, {}, {}], "count": 1});
+        pageCtrl = $controller('PaginationCtrl', {$scope: $scope });
+        httpBackend.flush();
+      }));
 
-        it('should have initial value', function() {
-            var value1 = element(by.binding('filterOptions.filterMana'));
-            expect(value1.getText()).toContain('15');
-        });
+      it('should have 2 cards with green filter', function () {
+          httpBackend.when("GET", "/api/collect/card/?limit=10&offset=0&color=Green&manalimit=15").respond({"results": [{}, {}], "count": 2});
+          $scope.filterOptions.filterColor.Green = true;
+          $scope.updateCards();
+          httpBackend.flush();
+          expect($scope.totalServerItems).toEqual(2);
+      });
 
-        it('should change value', function() {
-            var value1 = element(by.binding('filterOptions.filterMana'));
-            element(by.model('filterOptions.filterMana')).val('3');
-            expect(value1.getText()).toContain('3');
-        });
+      it('should have 5 cards with black filter', function () {
+          httpBackend.when("GET", "/api/collect/card/?limit=10&offset=0&color=Black&manalimit=15").respond({"results": [{}, {}, {}], "count": 5});
+          $scope.filterOptions.filterColor.Black = true;
+          $scope.updateCards();
+          httpBackend.flush();
+          expect($scope.totalServerItems).toEqual(5);
+      });
 
-    });
+      it('should have 1 cards with white filter', function () {
+          httpBackend.when("GET", "/api/collect/card/?limit=10&offset=0&color=White&manalimit=15").respond({"results": [{}], "count": 1});
+          $scope.filterOptions.filterColor.White = true;
+          $scope.updateCards();
+          httpBackend.flush();
+          expect($scope.totalServerItems).toEqual(1);
+      });
 
-    describe('pagination', function(){
+      it('should have 6 cards with red filter', function () {
+          httpBackend.when("GET", "/api/collect/card/?limit=10&offset=0&color=Red&manalimit=15").respond({"results": [{}], "count": 6});
+          $scope.filterOptions.filterColor.Red = true;
+          $scope.updateCards();
+          httpBackend.flush();
+          expect($scope.totalServerItems).toEqual(6);
+      });
 
-        it('pagination buttons should update model', function() {
-            var text = $('#card_browse').find('tbody tr').first().text();
-            var value1 = element(by.id('pagination-next ng-scope'));
-            value1.click();
-            expect( $('#card_browse').find('tbody tr').all()).not.toContain(text);
+      it('should have 8 cards with blue filter', function () {
+          httpBackend.when("GET", "/api/collect/card/?limit=10&offset=0&color=Blue&manalimit=15").respond({"results": [{}], "count": 8});
+          $scope.filterOptions.filterColor.Blue = true;
+          $scope.updateCards();
+          httpBackend.flush();
+          expect($scope.totalServerItems).toEqual(8);
+      });
 
-            var newText = $('#card_browse').find('tbody tr').first().text();
-            expect(text).not.toMatch(newText);
+      it('should have 7 cards with colorless filter', function () {
+          httpBackend.when("GET", "/api/collect/card/?limit=10&offset=0&color=Colorless&manalimit=15").respond({"results": [{}], "count": 7});
+          $scope.filterOptions.filterColor.Colorless = true;
+          $scope.updateCards();
+          httpBackend.flush();
+          expect($scope.totalServerItems).toEqual(7);
+      });
 
-            value1 = element(by.id('pagination-prev ng-scope'));
-            value1.click();
-
-            expect( $('#card_browse').find('tbody tr').all()).toContain(text);
-
-            newText = $('#card_browse').find('tbody tr').first().text();
-            expect(text).toMatch(newText);
-        });
-
-    });
-
-    describe('generateApiString function', function(){
-
-        it('generateApiString should be default configuration', function() {
-
-        });
-
-        it('generateApiString should be form correct api string', function() {
-
-        });
-    });
-
-    describe('$watch functions', function(){
-
-        it('getPagedDataAsync called when page changed', function() {
-
-        });
-
-        it('getPagedDataAsync called when color checkbox checked', function() {
-
-        });
-
-        it('getPagedDataAsync called when mana limit spinner changed', function() {
-
-        });
-
-        it('getPagedDataAsync called Name column sorted', function() {
-
-        });
-    });
-
-    describe('generateFilterColor function', function(){
-
-        it('return string should be blank with no colors checked', function() {
-
-        });
-
-        it('return string should contain black and blue', function() {
-
-        });
-    });
-
-    describe('getPagedDataAsync function', function(){
-
-        it('default load should have nonzero total server items', function() {
-
-        });
-*/
+      it('should have 9 cards with colorless and red filter', function () {
+          httpBackend.when("GET", "/api/collect/card/?limit=10&offset=0&color=Colorless,Red&manalimit=15").respond({"results": [{}], "count": 9});
+          $scope.filterOptions.filterColor.Colorless = true;
+          $scope.filterOptions.filterColor.Red = true;
+          $scope.updateCards();
+          httpBackend.flush();
+          expect($scope.totalServerItems).toEqual(9);
+      });
+  });
 });

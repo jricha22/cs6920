cbd9929fdbfcb360ad5c73f4d8aca8e9138a34d5
wdaivds1 <wdavids1@my.westga.edu>
2016-07-10 09:15:05 -0600

    Corrected issue #41

--- front_end/app/view2/view2.js
+++ front_end/app/view2/view2.js
@@ -6,18 +6,9 @@ angular.module('myApp.view2', ['ngRoute'])
     $routeProvider.when('/view2', {
         templateUrl: 'static/view2/view2.html',
         controller: 'View2Ctrl',
-        resolve: {
-            authorize: ['$rootScope', "$window", function ($rootScope, $window) {
-                if (!$rootScope.profile) {
-                    $window.location.href = '/#!/login_required';
-                    throw 302; // THIS ERROR as HTTP 302 Found
-                    return;
-                }
-
-            }]
-        }
     });
 }])
+
 .controller('View2Ctrl', function($scope, $http) {
     
     $scope.updateCards = function () {
